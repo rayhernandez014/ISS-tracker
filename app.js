528c1f4a4a1d9edc7bb75cb5c015ba9bd682eba1
Ray Hernandez <rayhernandez014@gmail.com>
2022-07-20 19:51:27 -0400

    uibuild

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const authRouter = require('./controllers/auth')
 const middleware = require('./utils/middleware')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
 app.use(middleware.requestLogger)
