7d93dfd0402a850199c42c64c75eeb40d7ca7821
Ray Hernandez <rayhernandez014@gmail.com>
2022-08-15 22:20:00 -0400

    improved error handling on backend

--- controllers/location.js
+++ controllers/location.js
@@ -2,8 +2,16 @@ const locationRouter = require('express').Router()
 const cronModule = require('../index')
 
 locationRouter.get('/', (request, response) => {
-  const location = { location: cronModule.location() }
-  response.json(location)
+  const consultedLocation = cronModule.location()
+  //const consultedLocation = undefined
+  if (consultedLocation) {
+    const location = { location: consultedLocation }
+    response.json(location)
+  }
+  else {
+    response.status(503).json({ error: 'Location information unavailable' })
+  }
+
 })
 
 module.exports = locationRouter
